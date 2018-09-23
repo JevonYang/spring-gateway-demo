125f5070e4e452d6125227e3a6aeedae42fd3254
JevonYang <yangzijing08@gmail.com>
2018-09-23 23:33:37 +0800

    增加redis限流配置

--- src/main/java/com/yang/gateway/RouterConfig.java
+++ src/main/java/com/yang/gateway/RouterConfig.java
@@ -3,10 +3,12 @@ package com.yang.gateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.DispatcherHandler;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
