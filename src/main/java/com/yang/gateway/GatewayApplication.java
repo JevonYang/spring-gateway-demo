f2a2e78af5d5c3067e0462e2b93a8ca05835002b
Jevon Yang <yangzijing08@gmail.com>
2019-01-22 18:58:50 +0800

    polish - 注释掉redisson

--- src/main/java/com/yang/gateway/GatewayApplication.java
+++ src/main/java/com/yang/gateway/GatewayApplication.java
@@ -2,10 +2,8 @@ package com.yang.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// @ComponentScan(basePackages = {"com.yang.gateway.*"})
 public class GatewayApplication {
 
     public static void main(String[] args) {
